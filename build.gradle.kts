4c5c6b1ae518989f7a6e0f876e0205ca370653c4
Nicholas Doglio <NicholasDoglio@Gmail.com>
2019-01-12 12:12:39 -0500

    Upgraded to AGP 3.4.0-alpha10

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0-alpha09")
+        classpath("com.android.tools.build:gradle:3.4.0-alpha10")
         classpath(kotlin("gradle-plugin", version = "1.3.11"))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
