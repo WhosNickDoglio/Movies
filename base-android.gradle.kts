df01018895f58715606be219da3d5ae3a6011a29
Nicholas Doglio <NicholasDoglio@Gmail.com>
2019-01-12 12:18:11 -0500

    Added MIT License to files

--- base-android.gradle.kts
+++ base-android.gradle.kts
@@ -1,5 +1,29 @@
 /*
-This doesn't work  ¯\_(ツ)_/¯
+ * MIT License
+ *
+ * Copyright (c) 2019 Nicholas Doglio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+/*This doesn't work  ¯\_(ツ)_/¯
 
 Waiting on more information
 
@@ -7,53 +31,46 @@ https://stackoverflow.com/questions/53604782/how-to-create-a-base-gradle-file-in
 
 
 https://github.com/gradle/kotlin-dsl/issues/1287
+*/
 
-
-
-
-
-import com.android.build.gradle.LibraryExtension
-import com.nicholasdoglio.buildsrc.App
-import com.nicholasdoglio.buildsrc.Libs
-
-apply {
-    plugin("com.android.library")
-    plugin("org.jetbrains.kotlin.android")
-    plugin("org.jetbrains.kotlin.kapt")
-}
-
-configure<LibraryExtension> {
-    compileSdkVersion(App.compileSdk)
-    defaultConfig {
-        minSdkVersion(App.minSdk)
-        targetSdkVersion(App.targetSdk)
-        versionCode = App.versionCode
-        versionName = App.versionName
-        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-dependencies {
-    val implementation by configurations
-    val testImplementation by configurations
-    val androidTestImplementation by configurations
-
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation(Libs.kotlin_stdlib_jdk8)
-    implementation(Libs.appcompat_v7)
-    testImplementation(Libs.junit)
-    androidTestImplementation(Libs.com_android_support_test_runner)
-    androidTestImplementation(Libs.espresso_core)
-}*/
+//plugins {
+//    id("com.android.library")
+//    kotlin("kotlin.android")
+//    kotlin("kapt")
+//}
+//
+//android {
+//    compileSdkVersion(App.compileSdk)
+//    defaultConfig {
+//        minSdkVersion(App.minSdk)
+//        targetSdkVersion(App.targetSdk)
+//        versionCode = App.versionCode
+//        versionName = App.versionName
+//        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+//    }
+//
+//    buildTypes {
+//        getByName("release") {
+//            isMinifyEnabled = false
+//            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+//        }
+//    }
+//
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
+//    }
+//}
+//
+//dependencies {
+//    val implementation by configurations
+//    val testImplementation by configurations
+//    val androidTestImplementation by configurations
+//
+//    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+//    implementation(Libs.kotlin_stdlib_jdk8)
+//    implementation(Libs.appcompat)
+//    testImplementation(Libs.junit)
+//    androidTestImplementation(Libs.androidx_test_runner)
+//    androidTestImplementation(Libs.espresso_core)
+//}
